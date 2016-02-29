49f8910334ab0c9277ad0c5bd9c069d3f253b8f8
Aaron Groch <ar.groch@gmail.com>
2016-02-29 13:19:47 -0500

    added tweet modal to navbar, also made edit profile and sign out links part of dropdown in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :new_tweet
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def new_tweet
+    @tweet = Tweet.new
+  end
 
   protected
 
